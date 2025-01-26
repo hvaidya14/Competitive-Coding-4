10c4aa2bcb05c443d6cd37d2f7709ce73f3ea3c6
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-01-25 17:15:38 -0800

    Update sample.java

--- sample.java
+++ sample.java
@@ -5,3 +5,48 @@
 
 
 // Your code here along with comments explaining your approach
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    boolean isvalid = true;
+    public boolean isBalanced(TreeNode root) {
+        postorder(root);
+        return isvalid;
+    }
+    private int postorder(TreeNode root) {
+
+        if ( root == null) { // base condition
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 0;
+        }          
+        int lefth = postorder(root.left); 
+        if (root.left != null) {
+            lefth=lefth+1;
+        }
+        int righth = postorder(root.right); //  root = 3 1+2; = 3  3-1 2
+        if (root.right != null) {
+            righth = righth+1;
+        }
+        int diff = Math.abs(lefth - righth); //  2-1 = 1
+        System.out.println(diff);
+        if (diff > 1) {
+            isvalid = false;
+        }
+        return Math.max(lefth, righth); // 3>1 3
+    }
+}
