39e7fd930145e9ebc8af35d4624bec8c66a5439e
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-01-28 14:01:52 -0800

    Update sample.java

--- sample.java
+++ sample.java
@@ -50,3 +50,65 @@ class Solution {
         return Math.max(lefth, righth); // 3>1 3
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    boolean xfound=false;
+    boolean yfound=false;        
+    Queue<TreeNode> q = new LinkedList<TreeNode>();
+    public boolean isCousins(TreeNode root, int x, int y) {
+        q.add(root);
+        return findCousins(root, x, y);
+    }
+
+    private boolean findCousins(TreeNode root, int x, int y) {
+        if (root == null) {
+            return false;
+        }
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i=0;i<size;i++) {
+                TreeNode curr = q.poll();
+                if (curr.left != null && curr.right != null) {
+                    if (curr.left.val == x && curr.right.val == y) {
+                        return false;
+                    }else if (curr.right.val == x && curr.left.val == y) {
+                        return false;
+                    }
+                }
+                if (curr.val == x) {
+                    xfound=true;
+                }
+                if (curr.val == y) {
+                    yfound=true;
+                }
+                if (curr.left != null) {
+                    q.add(curr.left);
+                }
+                if (curr.right != null) {
+                    q.add(curr.right);
+                }
+            }
+            if (xfound == true && yfound == true) {
+                return true;
+            } else if (xfound == true || yfound == true) {
+                return false;
+            }
+        }
+        return false;
+    }
+}
